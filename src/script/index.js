5d4f729256d52d9a4d30f7490e8e06ce6b1dcf4c
quentinformatique <quentin161204@gmail.com>
2023-11-22 00:12:37 +0100

    modified to change css to sass

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,10 @@
 const TL = gsap.timeline({ paused: false });
 
-TL.to(".text-gradient", {
+TL.from(".line", {
     duration: 1,
-    x: 200,
+    x: -500,
+    opacity: 0,
+    ease: Power2.easeInOut,
+    stagger: .1
 });
 
