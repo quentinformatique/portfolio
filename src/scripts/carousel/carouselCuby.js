a5484e65f0d2934e31329c09d779625f75ace29e
quentinformatique <quentin161204@gmail.com>
2023-11-26 17:05:44 +0100

    carousel wip

--- src/scripts/carousel/carouselCuby.js
+++ src/scripts/carousel/carouselCuby.js
@@ -1,12 +1,12 @@
-let count = 0;
+let countCuby = 0;
 const targets = document.querySelectorAll(".img-cuby");
 gsap.set(targets, { xPercent: -100 });
 gsap.set(targets[0], { xPercent: 0 });
 
 function slideIt() {
-    gsap.to(targets[count], { xPercent: 100 });
-    count = count < targets.length - 1 ? ++count : 0;
-    gsap.fromTo(targets[count], { xPercent: -100 }, { xPercent: 0 });
+    gsap.to(targets[countCuby], { xPercent: 100 });
+    countCuby = countCuby < targets.length - 1 ? ++countCuby : 0;
+    gsap.fromTo(targets[countCuby], { xPercent: -100 }, { xPercent: 0 });
     gsap.to({}, { duration: 2.5, onComplete: slideIt });
 }
 
